ae0274f655f6b0bdfb2000be00a4ba38f398e85b
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-28 22:19:35 +0700

    feat: Menambahkan migration dan model untuk tabel Products

---
+++ database/migrations/2025_05_28_145605_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('name', 150);
+            $table->decimal('price', 12,2)->unsigned();
+            $table->unsignedInteger('stock_quantity');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
