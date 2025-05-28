c48cbaff006c118d3c2bb4178b37424976ebe90e
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-28 21:55:12 +0700

    feat: Menambahkan migration dan model untuk tabel Categories

---
+++ app/Models/CategoriesModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriesModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categories';
+    protected $primaryKey = 'categories_id';
+
+    protected $fillable = ['name'];
+}
